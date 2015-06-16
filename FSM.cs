9aeb0fd535dd24fc82d79d30a3ce462aba170534
GitHubOfSalvatore <kumarrahulbhadani@gmail.com>
2015-06-17 03:42:54 +0530

    Second Commit

---
+++ FSM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRT_Transcript
+{
+    public enum eSTATECODE { STATE_INITIAL, STATE_NUM, STATE_TIME, STATE_ALPHA, STATE_BLANK, STATE_ACCEPT, STATE_REJECT, STATE_MAX };
+    public enum eEVENTCODE
+    {
+        EVENT_NUM, EVENT_TIME, EVENT_ALPHA, EVENT_BLANK, EVENT_NOT_NUM, EVENT_NOT_TIME, EVENT_NOT_ALPHA,
+        EVENT_NOT_BLANK, EVENT_EOF, EVENT_MAX
+    };
+    
+    //public delegate void fptrStateFunc
+    public delegate bool fptrStateFunc(string s);
+    public struct sTransRules
+    {
+        private eSTATECODE eSTATECODE1;
+        private eEVENTCODE eEVENTCODE;
+        private eSTATECODE eSTATECODE2;
+
+        public sTransRules(eSTATECODE eSTATECODE1, eEVENTCODE eEVENTCODE, eSTATECODE eSTATECODE2)
+        {
+            // TODO: Complete member initialization
+            this.eSTATECODE1 = eSTATECODE1;
+            this.eEVENTCODE = eEVENTCODE;
+            this.eSTATECODE2 = eSTATECODE2;
+        }
+    };
+
+    class FSM
+    {
+        private sTransRules[] sRULE = 
+        {
+            new sTransRules(eSTATECODE.STATE_INITIAL, eEVENTCODE.EVENT_NUM, eSTATECODE.STATE_NUM),
+            new sTransRules(eSTATECODE.STATE_INITIAL, eEVENTCODE.EVENT_NOT_NUM, eSTATECODE.STATE_REJECT),
+
+            new sTransRules(eSTATECODE.STATE_NUM, eEVENTCODE.EVENT_TIME, eSTATECODE.STATE_TIME),
+            new sTransRules(eSTATECODE.STATE_NUM, eEVENTCODE.EVENT_NOT_TIME, eSTATECODE.STATE_REJECT),
+
+            new sTransRules(eSTATECODE.STATE_TIME, eEVENTCODE.EVENT_ALPHA, eSTATECODE.STATE_ALPHA),
+            new sTransRules(eSTATECODE.STATE_TIME, eEVENTCODE.EVENT_NOT_ALPHA, eSTATECODE.STATE_REJECT),
+
+            new sTransRules(eSTATECODE.STATE_ALPHA, eEVENTCODE.EVENT_BLANK, eSTATECODE.STATE_BLANK),
+            new sTransRules(eSTATECODE.STATE_ALPHA, eEVENTCODE.EVENT_NOT_BLANK, eSTATECODE.STATE_REJECT),
+
+            new sTransRules(eSTATECODE.STATE_BLANK, eEVENTCODE.EVENT_NUM, eSTATECODE.STATE_NUM),
+            new sTransRules(eSTATECODE.STATE_BLANK, eEVENTCODE.EVENT_EOF, eSTATECODE.STATE_ACCEPT),
+
+        };
+
+        //A method to validate numbers
+        public bool bSTATE_NUM_FN(string s)
+        {
+            int i = 0;
+            bool bResult = int.TryParse(s, out i);
+            return bResult;
+        }
+
+        public bool bSTATE_BLANK_FN(string s)
+        {
+            bool bResult = String.Equals(s, "");
+            return bResult;
+        }
+
+        public bool bSTATE_TIME_FN(string s)
+        {
+            bool bResult = false;
+            string sPattern = @"[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]";
+            if (System.Text.RegularExpressions.Regex.IsMatch(s, sPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            {
+                bResult = true;
+            }
+            return bResult;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SRT_Transcript
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void processDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Drop Ocurred");
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string sFile in files)
+                ValidateAndConverSRT(sFile);
+        }
+        private void ValidateAndConverSRT(string fileName)
+        {
+            string sExtension;
+            string sDirectory;
+            string sTranscript = "";
+            // Open the file to read from. 
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                sExtension = System.IO.Path.GetExtension(fileName);
+                sDirectory = System.IO.Path.GetDirectoryName(fileName);
+                Console.WriteLine(sDirectory);
+                Console.WriteLine(sExtension);
+                if (!String.Equals(sExtension, ".srt", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Dropped file is not a subtitle file");
+                }
+                else
+                {
+                    FSM cStateMachine = new FSM();
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+
+                        //STATE_INITIAL, STATE_NUM, STATE_TIME, STATE_ALPHA, STATE_BLANK, STATE_ACCEPT, STATE_REJECT, STATE_MAX
+                        /*fptrStateFunc[] StateFuncArr = { 
+                                                           cStateMachine.bSTATE_NUM_FN, cStateMachine.bSTATE_TIME_FN, 
+                                                           cStateMachine.bSTATE_ALPHA_FN, cStateMachine.bSTATE_BLANK_FN, 
+                                                           cStateMachine.bSTATE_ACCEPT, cStateMachine.bSTATE_REJECT
+                                                       };
+                        */
+                        if (cStateMachine.bSTATE_NUM_FN(s) || cStateMachine.bSTATE_BLANK_FN(s) || cStateMachine.bSTATE_TIME_FN(s))
+                            continue;
+                        else
+                            sTranscript = sTranscript + s;
+                    }
+                }
+            }
+            string sTransFileName = @sDirectory + @"\" + System.IO.Path.GetFileNameWithoutExtension(fileName) + ".txt";
+            // Append a number if file exists. 
+            try
+            {
+
+                if (File.Exists(sTransFileName))
+                {
+                    sTransFileName = @sDirectory + @"\" +  System.IO.Path.GetFileNameWithoutExtension(fileName) + "_SRTTrans" + ".txt";
+                    if (File.Exists(sTransFileName)) { File.Delete(sTransFileName); }
+                }
+                // Create the file. 
+                using (FileStream fs = File.Create(sTransFileName))
+                {
+                    Byte[] bInfo = new UTF8Encoding(true).GetBytes(sTranscript);
+                    fs.Write(bInfo, 0, bInfo.Length);
+                }
+
+                sExtension = System.IO.Path.GetExtension(sTransFileName);
+                sDirectory = System.IO.Path.GetDirectoryName(sTransFileName);
+                Console.WriteLine(sDirectory);
+                Console.WriteLine(sExtension);
+
+                // Open the stream and read it back. 
+                using (StreamReader sr = File.OpenText(sTransFileName))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                       // Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void openFile(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // Set filter options and filter index.
+            openFileDialog.Filter = "Text documents (.srt)|*.srt"; // Filter files by extension 
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = true;
+            bool? bUserClickOK = openFileDialog.ShowDialog();
+            if (bUserClickOK == true)
+            {
+                foreach (string sFile in openFileDialog.FileNames)
+                {
+                    ValidateAndConverSRT(sFile);
+                }
+            }
+
+        }
+    }
+
+}
